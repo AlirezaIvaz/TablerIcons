adcd594c3a85b0a9135d59eea16dd111890ae01e
Alireza Ivaz <ivaz1382@gmail.com>
2024-11-20 11:14:18 +0330

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,9 +64,10 @@ android {
 
 dependencies {
     implementation(project(":library"))
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.browser:browser:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
