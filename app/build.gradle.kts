391fa3fbd5798d25133cfd2c56bd63af59fc4773
Alireza Ivaz <ivaz1382@gmail.com>
2024-11-21 10:35:55 +0330

    Customize debug build type

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,10 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
     }
     flavorDimensions += "distributor"
     productFlavors {
